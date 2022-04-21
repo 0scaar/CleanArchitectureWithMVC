b1e7679e53e96a92c5f534258529c3d61c41a54d
Oscar <oscaar_2003@hotmail.com>
2022-04-21 02:25:31 -0300

    adding product index

---
+++ CleanArch/CleanArch.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await productService.GetProducts();
+            return View(result);
+        }
+    }
+}
